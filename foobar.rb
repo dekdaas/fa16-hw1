4e8351abbe2a073feca50d9050c2ef2cf0195e27
Albert Pham <albert.p2012@gmail.com>
2016-09-21 14:24:40 -0700

    'Finished'

--- foobar.rb
+++ foobar.rb
@@ -1,8 +1,8 @@
 class Foobar
-
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map{|e| e.to_i + 2}.keep_if{|e| e % 2 == 0}.uniq.delete_if{|e| e > 10}.reduce(:+)
   end
 end
 
